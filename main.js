90594b0275044225f903f6e6bcd7f3910f788525
Your Name <you@example.com>
2023-05-20 20:48:17 +0100

    😍

---
+++ main.js
@@ -0,0 +1,10 @@
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+
+let form = document.getElementById('form')
+let link  = document.getElementById('url')
+
+let img = document.getElementById('img')
+form.addEventListener('submit' , (e)=>{
+  e.preventDefault()
+  img.setAttribute('src' ,"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="  + link.value )
+})
