ae32ba6911c4614b47fd620862486d05ce6c6dce
Yassine <110790693+Yassin6up@users.noreply.github.com>
2023-06-17 00:51:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let img = document.getElementById('img')
 form.addEventListener('submit' , (e)=>{
   e.preventDefault()
   img.setAttribute('src' ,"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="  + link.value )
-  document.getElementById("download").setAttribute("href" , link.value)
+  document.getElementById("download").setAttribute("href" ,"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="  + link.value )
   document.getElementById("download").click() ;
+  
+  // document.getElementById("imgdonloader").
 })
